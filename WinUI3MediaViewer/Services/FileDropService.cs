e5dc30b0e65b488168b6975e8b0e260b0261ff58
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:53:09 +0800

    Update FileDropService.cs

--- WinUI3MediaViewer/Services/FileDropService.cs
+++ WinUI3MediaViewer/Services/FileDropService.cs
@@ -1,1 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using WinUI3MediaViewer.Core.Interfaces;
+using WinUI3MediaViewer.Helpers;
 
+namespace WinUI3MediaViewer.Services;
+
+public class FileDropService : IFileDropService
+{
+    public IEnumerable<string> GetSupportedFiles(IEnumerable<string> filePaths, bool includeSubfolders = true)
+    {
+        var result = new List<string>();
+
+        foreach (var path in filePaths)
+        {
+            if (File.Exists(path))
+            {
+                if (FileTypeHelper.IsSupported(path))
+                    result.Add(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                var searchOption = includeSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                foreach (var file in Directory.EnumerateFiles(path, "*.*", searchOption))
+                {
+                    if (FileTypeHelper.IsSupported(file))
+                        result.Add(file);
+                }
+            }
+        }
+
+        return result;
+    }
+}
