6da143a2b06fa91b1cc29ef02e074a9982447553
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:51:20 +0800

    Update ShellHelper.cs

--- WinUI3MediaViewer/Helpers/ShellHelper.cs
+++ WinUI3MediaViewer/Helpers/ShellHelper.cs
@@ -1,1 +1,16 @@
+using System.Diagnostics;
+using System.IO;
 
+namespace WinUI3MediaViewer.Helpers;
+
+public static class ShellHelper
+{
+    public static void OpenInFileExplorer(string filePath)
+    {
+        var folder = Path.GetDirectoryName(filePath);
+        if (!string.IsNullOrEmpty(folder) && Directory.Exists(folder))
+        {
+            Process.Start("explorer.exe", $"/select,\"{filePath}\"");
+        }
+    }
+}
