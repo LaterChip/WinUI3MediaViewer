304c3cbaaa949159ffde8f1fdbff3b049876c26f
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:49:32 +0800

    Update IPlaybackService.cs

--- WinUI3MediaViewer/Core/Interfaces/IPlaybackService.cs
+++ WinUI3MediaViewer/Core/Interfaces/IPlaybackService.cs
@@ -1,1 +1,12 @@
+namespace WinUI3MediaViewer.Core.Interfaces;
 
+public interface IPlaybackService
+{
+    void Play();
+    void Pause();
+    void Stop();
+    void SetSource(string filePath);
+    double Position { get; set; }
+    double Duration { get; }
+    bool IsPlaying { get; }
+}
