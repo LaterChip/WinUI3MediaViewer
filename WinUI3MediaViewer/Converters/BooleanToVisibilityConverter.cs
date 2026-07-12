ecd41853a00f0e3237bfaa63bd7697294d795714
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:52:22 +0800

    Update BooleanToVisibilityConverter.cs

--- WinUI3MediaViewer/Converters/BooleanToVisibilityConverter.cs
+++ WinUI3MediaViewer/Converters/BooleanToVisibilityConverter.cs
@@ -1,1 +1,23 @@
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
 
+namespace WinUI3MediaViewer.Converters;
+
+public class BooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool b)
+        {
+            bool invert = parameter?.ToString()?.ToLower() == "invert";
+            return (b ^ invert) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
