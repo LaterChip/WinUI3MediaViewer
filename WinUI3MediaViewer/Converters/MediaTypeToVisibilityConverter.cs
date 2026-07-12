5f2e3c4e1764002d7dfa4e4a8fc2a20cc50e0b47
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:51:37 +0800

    Update MediaTypeToVisibilityConverter.cs

--- WinUI3MediaViewer/Converters/MediaTypeToVisibilityConverter.cs
+++ WinUI3MediaViewer/Converters/MediaTypeToVisibilityConverter.cs
@@ -1,1 +1,30 @@
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using WinUI3MediaViewer.Core.Enums;
 
+namespace WinUI3MediaViewer.Converters;
+
+public class MediaTypeToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is MediaType type && parameter is string targetTypeStr)
+        {
+            var expected = targetTypeStr.ToLowerInvariant() switch
+            {
+                "image" => MediaType.Image,
+                "video" => MediaType.Video,
+                "audio" => MediaType.Audio,
+                _ => MediaType.Unsupported
+            };
+            return type == expected ? Visibility.Visible : Visibility.Collapsed;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
