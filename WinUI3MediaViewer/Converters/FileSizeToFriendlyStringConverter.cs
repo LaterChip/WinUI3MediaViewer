290754db34f560fd51820856b297a70c05e11666
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:52:49 +0800

    Update FileSizeToFriendlyStringConverter.cs

--- WinUI3MediaViewer/Converters/FileSizeToFriendlyStringConverter.cs
+++ WinUI3MediaViewer/Converters/FileSizeToFriendlyStringConverter.cs
@@ -1,1 +1,29 @@
+using System;
+using Microsoft.UI.Xaml.Data;
 
+namespace WinUI3MediaViewer.Converters;
+
+public class FileSizeToFriendlyStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is long size)
+        {
+            string[] suffixes = { "B", "KB", "MB", "GB", "TB" };
+            int index = 0;
+            double dSize = size;
+            while (dSize >= 1024 && index < suffixes.Length - 1)
+            {
+                dSize /= 1024;
+                index++;
+            }
+            return $"{dSize:0.##} {suffixes[index]}";
+        }
+        return "0 B";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
