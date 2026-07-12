5b0a6d869d09e8891f894286d8f63962526b6f9a
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:48:21 +0800

    Update App.xaml.cs

--- WinUI3MediaViewer/App.xaml.cs
+++ WinUI3MediaViewer/App.xaml.cs
@@ -1,1 +1,19 @@
+using Microsoft.UI.Xaml;
 
+namespace WinUI3MediaViewer;
+
+public partial class App : Application
+{
+    private Window? m_window;
+
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        m_window = new Views.MainWindow();
+        m_window.Activate();
+    }
+}
