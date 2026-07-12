cd2621602461cf3ffc34573e2501e924350a91bf
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:48:59 +0800

    Update IFileDropService.cs

--- WinUI3MediaViewer/Core/Interfaces/IFileDropService.cs
+++ WinUI3MediaViewer/Core/Interfaces/IFileDropService.cs
@@ -1,1 +1,11 @@
+using System.Collections.Generic;
 
+namespace WinUI3MediaViewer.Core.Interfaces;
+
+public interface IFileDropService
+{
+    /// <summary>
+    /// 从拖放数据中提取所有支持的媒体文件路径。
+    /// </summary>
+    IEnumerable<string> GetSupportedFiles(IEnumerable<string> filePaths, bool includeSubfolders = true);
+}
