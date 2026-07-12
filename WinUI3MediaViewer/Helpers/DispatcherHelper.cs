94bdc1834a19f93185d28258ce028497ca31c8f5
LaterChip <bfgh12345@outlook.com>
2026-07-12 12:51:03 +0800

    Update DispatcherHelper.cs

--- WinUI3MediaViewer/Helpers/DispatcherHelper.cs
+++ WinUI3MediaViewer/Helpers/DispatcherHelper.cs
@@ -1,1 +1,25 @@
+using System;
+using Microsoft.UI.Dispatching;
 
+namespace WinUI3MediaViewer.Helpers;
+
+public static class DispatcherHelper
+{
+    private static DispatcherQueue? _dispatcher;
+
+    public static void Initialize(DispatcherQueue dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    public static void RunOnUIThread(Action action)
+    {
+        if (_dispatcher is null)
+            throw new InvalidOperationException("Dispatcher not initialized.");
+
+        if (_dispatcher.HasThreadAccess)
+            action();
+        else
+            _dispatcher.TryEnqueue(() => action());
+    }
+}
